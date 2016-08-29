2646933c2ec9be0fafe51072b85d85e54fc2dbf1
Andrew Golightly <andrew@golightlyplus.com>
2016-08-29 15:23:51 +0700

    fetching, collating and outputting movie data

--- get-movies.js
+++ get-movies.js
@@ -5,13 +5,13 @@ const cheerio = require('cheerio');
 
 const movieData = {};
 
+// collate the movie data
 request('https://booking.sfcinemacity.com/visPrintShowTimes.aspx?visLang=1&visCinemaId=9936', (err, httpResponse, body) => {
   const $ = cheerio.load(body);
 
   let currentMovie;
   let currentDate;
   $('#tblShowTimes td').each(function () {
-    //console.log($(this).text());
     if ($(this).hasClass('PrintShowTimesFilm')) {
       currentMovie = $(this).text();
       movieData[currentMovie] = {};
@@ -22,5 +22,18 @@ request('https://booking.sfcinemacity.com/visPrintShowTimes.aspx?visLang=1&visCi
     }
   });
 
-  console.log(movieData);
+  // test out the data by showing movie info for x days ahead..
+  const daysAhead = 1;
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + daysAhead);
+
+  console.log(`Target date is ${targetDate.toDateString()}`);
+
+  for (const movieName of Object.keys(movieData)) {
+    for (const movieDate of Object.keys(movieData[movieName])) {
+      if (movieDate.includes(` ${targetDate.getDate()} `)) {
+        console.log(`${movieName} is showing at ${movieData[movieName][movieDate]}`);
+      }
+    }
+  }
 });
