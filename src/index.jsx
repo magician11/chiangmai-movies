b16caea7995eb23f7c3372d4aa70b57e1e697ec0
Andrew Golightly <andrew@golightlyplus.com>
2016-08-30 15:24:18 +0700

    basic structure working

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Grid, Row, Col, Glyphicon } from 'react-bootstrap';
 import MovieTimes from './components/movie-times';
 
 require('bootstrap/dist/css/bootstrap.css');
 
 const root = document.createElement('div');
 document.body.appendChild(root);
-ReactDOM.render(<MovieTimes />, root);
+
+const basicInterface = (
+  <Grid>
+    <Row>
+      <Col xs={12}>
+        <div className="text-center">
+          <h1><Glyphicon glyph="film" /> Movie times</h1>
+          <h2>
+            <Glyphicon glyph="star" /> Maya Mall in Chiang Mai, Thailand <Glyphicon glyph="star" />
+          </h2>
+        </div>
+        <hr />
+        <MovieTimes />
+      </Col>
+    </Row>
+  </Grid>
+);
+
+ReactDOM.render(basicInterface, root);
