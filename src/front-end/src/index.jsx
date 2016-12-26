4054df73e829fe902e343d262ac9bd31167ad5fc
Andrew Golightly <support@andrewgolightly.com>
2016-12-26 22:41:49 +0700

    added service worker

--- src/front-end/src/index.jsx
+++ src/front-end/src/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MovieApp from './components/movie-app';
+import serviceWorker from 'file!./sw.js';
+
+/* eslint-disable no-console */
 
 require('bootstrap/dist/css/bootstrap.css');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(serviceWorker).then((registration) => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch((err) => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 ReactDOM.render(<MovieApp />, document.getElementById('app'));
