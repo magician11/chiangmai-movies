9340ab6fb506ce06d9cf2956ed2085e68aa82b10
Andrew Golightly <support@andrewgolightly.com>
2016-12-28 14:29:29 +0700

    updated about content

--- src/front-end/src/components/footer.jsx
+++ src/front-end/src/components/footer.jsx
@@ -21,14 +21,15 @@ class Footer extends Component {
           onHide={() => this.setState({ showModal: false })}
         >
           <Modal.Header closeButton>
-            <Modal.Title id="about-modal">About Chiang Mai Movies</Modal.Title>
+            <Modal.Title id="about-modal">About the Chiang Mai Movies Webapp</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <h4>How It All Started</h4>
             <p>Trying to find showtimes for movies in Chiang Mai (Thailand) was a surprisingly difficult thing to do. So being a web developer, I decided to build an app that would provide everything I normally look for when I look up what is playing.</p>
             <p>Thus the app needed to clearly show me the showtimes, as well as the meta data I normally look up afterwards, such as: the trailer, Rotten Tomatoes score, reviews, synopsis, actors, and runtime.</p>
             <h4>How Does It Work Exactly?</h4>
-            <p>Everytime you load the page, the list of available movies and their showtimes are scraped from <a href="https://booking.sfcinemacity.com/visPrintShowTimes.aspx?visLang=1&visCinemaId=9936">the SF Cinema City booking system</a>. This data is then augmented with movie meta data which is periodically obtained from <a href="https://www.themoviedb.org/">The Movie DB</a>, <a href="http://www.omdbapi.com/">OMDb</a>, and <a href="http://www.rottentomatoes.com">Rotten Tomatoes</a> itself.</p>
+            <p>Everytime you load the page, the list of available movies and their showtimes are scraped from <a href="https://booking.sfcinemacity.com/visPrintShowTimes.aspx?visLang=1&visCinemaId=9936">the SF Cinema City booking system</a>. This data is then augmented with movie metadata which is periodically obtained from <a href="https://www.themoviedb.org/">The Movie DB</a>, <a href="http://www.omdbapi.com/">OMDb</a>, and <a href="http://www.rottentomatoes.com">Rotten Tomatoes</a> itself.</p>
+            <p>The movie listings are sorted by their Rotten Tomoatoes score (highest to lowest/unknown).</p>
             <h4>Questions/Feedback</h4>
             <p>I welcome all questions and feedback. The whole purpose of this app was to make it super easy for people to find out movie times. So if you have any suggestions or feedback to support this mission, you can get in touch with me <a href="http://golightlyplus.com/contact/">here</a>.</p>
           </Modal.Body>
